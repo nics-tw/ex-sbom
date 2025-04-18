85bc95535d9ce88e92b93466ec353efc5254c848
Dory Huang <drhuang0922@gmail.com>
2025-04-18 12:04:27 +0800

    feat: rename, and add delete API

---
+++ internal/handler/sbom/delete.go
@@ -0,0 +1,26 @@
+package sbom
+
+import (
+	"ex-s/util/msg"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func DeleteSBOM(c *gin.Context) {
+	name := c.Param("name")
+	if name == "" {
+		c.JSON(http.StatusBadRequest, gin.H{msg.RespErr: fmt.Sprintf(msg.ErrMissingParam, "name")})
+		return
+	}
+
+	if _, exists := SBOMs[name]; !exists {
+		c.JSON(http.StatusNotFound, gin.H{msg.RespErr: msg.ErrSBOMNotFound})
+		return
+	}
+
+	delete(SBOMs, name)
+
+	c.JSON(http.StatusOK, gin.H{msg.RespMsg: "ok"})
+}
--- util/msg/msg.go
+++ util/msg/msg.go
@@ -6,7 +6,10 @@ const (
 	RespData = "data"
 
 	ErrInvalidSBOM = "Unknown SBOM"
+	ErrSBOMNotFound = "SBOM not found"
 
 	ErrParsingJson = "Error parsing JSON:"
 	ErrParsingSPDX = "Error parsing SPDX SBOM"
+
+	ErrMissingParam = "Missing required parameter: %s"
 )
