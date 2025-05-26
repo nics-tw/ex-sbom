2bfd84d30dbeac7d5a2143f6cd25167c2eb8dbc1
Dory Huang <drhuang0922@gmail.com>
2025-05-26 17:59:35 +0800

    test(get name): unit test for extracting package name

---
+++ util/file/scan_test.go
@@ -0,0 +1,52 @@
+//go:build unit
+
+package file
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetPackageName(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "with path separator",
+			input:    "symfony/http-foundation",
+			expected: "http-foundation",
+		},
+		{
+			name:     "without path separator",
+			input:    "simple-package",
+			expected: "simple-package",
+		},
+		{
+			name:     "empty string",
+			input:    "",
+			expected: "",
+		},
+		{
+			name:     "multiple separators",
+			input:    "org/group/package-name",
+			expected: "package-name",
+		},
+		{
+			name:     "trailing separator",
+			input:    "package/",
+			expected: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := getPackageName(tt.input)
+			if !assert.Equal(t, tt.expected, result) {
+				t.Errorf("getPackageName(%q) = %q, want %q", tt.input, result, tt.expected)
+			}
+		})
+	}
+}
