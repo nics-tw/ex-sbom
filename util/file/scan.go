9ae06e5e47bf38bf8506e403fdb9b6910153f888
Dory Huang <drhuang0922@gmail.com>
2025-06-12 14:59:20 +0800

    doc(LICENSE): strict license declaration for all files

--- util/file/scan.go
+++ util/file/scan.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 國家資通安全研究院-前瞻研究籌獲中心 National Institute of Cyber Security(RA)
+// SPDX-License-Identifier: MIT
+// Licensed under the MIT License. See LICENSE file in the project root for license information.
+
 package file
 
 import (
